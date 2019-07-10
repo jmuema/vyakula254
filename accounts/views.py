bf309a860d18f49552dd732454a09224e6a045fb
josephmuema <jmuema95@gmail.com>
2019-07-10 08:42:54 +0300

    added the django forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import View, TemplateView, CreateView, UpdateView
+from django.contrib.auth import logout
+from .forms import RegisterForm
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Profile
+from django.contrib.messages.views import SuccessMessageMixin
 # Create your views here.
