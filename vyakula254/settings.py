1e4fdf26d428365b213c05e916f515b66a07a007
josephmuema <jmuema95@gmail.com>
2019-07-10 11:47:41 +0300

    fixed an error in settings file

--- vyakula254/settings.py
+++ vyakula254/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurants.apps.RestaurantsConfig',
+    'resturants.apps.ResturantsConfig',
     'accounts.apps.AccountsConfig',
 ]
 
