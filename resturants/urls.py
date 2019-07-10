c6a3d2ef655948192a2b84c8f03a2f4f281925d4
josephmuema <jmuema95@gmail.com>
2019-07-10 11:44:22 +0300

    fixed an error in settings file

--- resturants/urls.py
+++ resturants/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
+from django.conf.urls import url 
 from .views import RestaurantListView, RestaurantDetailView,\
     RestaurantCreateView, RestaurantUpdateView, RestaurantDeleteView,\
     MyPostView
 
 urlpatterns = [
-    path('', RestaurantListView.as_view(), name='home'),
-    path('create/', RestaurantCreateView.as_view(), name='create'),
-    path('<slug:slug>/', RestaurantDetailView.as_view(), name='detail'),
-    path('<slug:slug>/update', RestaurantUpdateView.as_view(), name='update'),
-    path('<slug:slug>/delete', RestaurantDeleteView.as_view(), name='delete'),
-    path('dashboard/myposts/', MyPostView.as_view(), name='my_posts'),
+    url('', RestaurantListView.as_view(), name='home'),
+    url('create/', RestaurantCreateView.as_view(), name='create'),
+    url('<slug:slug>/', RestaurantDetailView.as_view(), name='detail'),
+    url('<slug:slug>/update', RestaurantUpdateView.as_view(), name='update'),
+    url('<slug:slug>/delete', RestaurantDeleteView.as_view(), name='delete'),
+    url('dashboard/myposts/', MyPostView.as_view(), name='my_posts'),
 ]
