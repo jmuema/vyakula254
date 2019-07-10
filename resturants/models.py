9585295ad3c5fdf0ed9fc2af70a53a9fdcb8f8c8
josephmuema <jmuema95@gmail.com>
2019-07-10 10:21:18 +0300

    imported slugify to my models

--- resturants/models.py
+++ resturants/models.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
 
 # Create your models here.
