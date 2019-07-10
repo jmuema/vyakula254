3df840805323499ff2dba884deb40bd7b8885635
josephmuema <jmuema95@gmail.com>
2019-07-10 10:56:43 +0300

    imported comments and resturant models

--- resturants/views.py
+++ resturants/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404,redirect
+from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
+from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from .models import Restaurant, Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
