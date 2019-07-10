828b881842eaeb17cda601ef3955e9409acbcab7
josephmuema <jmuema95@gmail.com>
2019-07-10 13:40:17 +0300

    fixed an error in settings file

--- vyakula254/urls.py
+++ vyakula254/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('restaurants.urls')),
+    path('', include('resturants.urls')),
     path('accounts/', include('accounts.urls')),
 
 ]
