4ee90dc7e74c85aa4e18b16ac077394bd9caf152
josephmuema <jmuema95@gmail.com>
2019-07-10 10:11:01 +0300

    register my models in my resturants app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,20 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.contrib import admin
+from .models import Restaurant, Comment
 
-from django.test import TestCase
 
-# Create your tests here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['title', 'categories', 'user',
+                    'location', 'price', 'taste', 'persons', ]
+    search_fields = ['title']
+    list_filter = ['user', 'created_at']
+    list_per_page = 20
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user', 'created_at']
+    search_fields = ['text']
+    list_per_page = 20
+    date_hierarchy = 'created_at'
